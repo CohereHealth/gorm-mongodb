f6d5515771b72a7a0b329fcb1796069dfca9fca1
leandrohenriquecostaandrade-ctrl <leandro.henriquecostaandrade@coherehealth.com>
2026-05-21 16:42:04 -0300

    .

--- grails-datastore-gorm-mongodb/src/main/groovy/org/grails/datastore/mapping/mongo/NativeTransactionAttribute.groovy
+++ grails-datastore-gorm-mongodb/src/main/groovy/org/grails/datastore/mapping/mongo/NativeTransactionAttribute.groovy
@@ -17,6 +17,13 @@ class NativeTransactionAttribute extends RuleBasedTransactionAttribute {
     private static final long serialVersionUID = 1L
 
     /**
+     * Unique prefix used as transaction name to identify native transactions.
+     * Spring's TransactionInterceptor wraps our attribute in a DelegatingTransactionAttribute,
+     * so instanceof checks fail. The name survives the wrapping and can be checked in doBegin.
+     */
+    public static final String NATIVE_TX_NAME_PREFIX = "NATIVE_MONGO_TX::"
+
+    /**
      * Marker method to identify this as a native transaction attribute.
      * @return true always
      */
